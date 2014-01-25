f3194af044476ccad3753696c1f41637313b68b8
Jerome Indefenzo <jeromeindefenzo@gmail.com>
2014-01-26 00:25:58 +0800

    properly implemented player dissolve/disintegration

--- Assets/Scripts/DisintegratePlayer.cs
+++ Assets/Scripts/DisintegratePlayer.cs
@@ -5,13 +5,18 @@ using System.Collections;
 [RequireComponent(typeof(SkinnedMeshRenderer))]
 public class DisintegratePlayer : MonoBehaviour {
 
+	private bool disintegrating = false;
+
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Q)) Disintegrate();
+		if (disintegrating) {
+			float alpha = Mathf.Lerp(renderer.material.color.a, 0f, 2 * Time.deltaTime);
+			renderer.material.color = new Vector4 (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alpha);
+		}
 	}
 
 	void Disintegrate () {
-		particleSystem.Play();
-		float alpha = Mathf.Lerp(renderer.material.color.a, renderer.material.color.a, 5 * Time.deltaTime);
-		renderer.material.color = new Vector4 (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alpha);
+		if (particleSystem) particleSystem.Play();
+		disintegrating = true;
 	}
 }
