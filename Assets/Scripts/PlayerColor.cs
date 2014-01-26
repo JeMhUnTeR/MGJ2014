eb303ac4bb933e53221f1fdac9fb9493ad1c87bc
Jerome Indefenzo <jeromeindefenzo@gmail.com>
2014-01-26 13:49:36 +0800

    commit for revert test

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColorManager : MonoBehaviour {
+
+	public Material PlayerMat;
+
+	public enum PlayerColor { // so this shouldn't be here
+		Red,
+		Green,
+		Blue
+	}
+
+	void Update () {
+		// change player color based on outside variable player color
+		// where the hell will the player color state come from
+	}
+}
